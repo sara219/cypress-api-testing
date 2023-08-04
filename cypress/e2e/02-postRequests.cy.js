06f26a6af3179e2d2be73c800cea69f20b7489e9
sara219 <sara97abuawwad@gmail.com>
2023-08-04 23:03:28 +0300

    Approach 1: Hard Coded JSON Object

---
+++ cypress/e2e/02-postRequests.cy.js
@@ -0,0 +1,19 @@
+/// <reference types='cypress' />
+
+describe('POST CALLS', () => {
+  it('Approach 1: Hard Coded JSON Object ', () => {
+    const reqBody = {
+      tourist_name: 'Sara J.',
+      tourist_email: 'sara444@gmail.com',
+      tourist_location: 'PAS',
+    }
+
+    cy.request({
+      method: 'POST',
+      url: 'http://restapi.adequateshop.com/api/Tourist',
+      body: reqBody,
+    }).then((res) => {
+      expect(res.status).to.eql(201)
+    })
+  })
+})
