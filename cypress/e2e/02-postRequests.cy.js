dfe3114f352489384e844ba643971a5fb5068a5c
sara219 <sara97abuawwad@gmail.com>
2023-08-05 22:10:58 +0300

    Approach 2: Dynamically Generate JSON Object

--- cypress/e2e/02-postRequests.cy.js
+++ cypress/e2e/02-postRequests.cy.js
@@ -4,8 +4,8 @@ describe('POST CALLS', () => {
   it('Approach 1: Hard Coded JSON Object ', () => {
     const reqBody = {
       tourist_name: 'Sara J.',
-    //   change email with every call:: 
-      tourist_email: 'sara444@gmail.com',
+      //   change email with every call::
+      tourist_email: 'saraj22@gmail.com',
       tourist_location: 'PAS',
     }
 
@@ -15,6 +15,23 @@ describe('POST CALLS', () => {
       body: reqBody,
     }).then((res) => {
       expect(res.status).to.eql(201)
+    })
+  })
+
+  it.only('Approach 2: Dynamically Generate JSON Object ', () => {
+    const reqBody = {
+      tourist_name: Math.random().toString(5).substring(2),
+      tourist_email: `${Math.random().toString(5).substring(2)}@gmail.com`,
+      tourist_location: 'PAS',
+    }
+
+    cy.request({
+      method: 'POST',
+      url: 'http://restapi.adequateshop.com/api/Tourist',
+      body: reqBody,
+    }).then((res) => {
+      expect(res.status).to.eql(201)
+      expect(res.body.tourist_name).to.eql(reqBody.tourist_name)
     })
   })
 })
