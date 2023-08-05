b67081f14c8ba25244aaa9defc400014d4e116bc
sara219 <sara97abuawwad@gmail.com>
2023-08-05 21:44:12 +0300

    add comment for future test|

--- cypress/e2e/02-postRequests.cy.js
+++ cypress/e2e/02-postRequests.cy.js
@@ -4,6 +4,7 @@ describe('POST CALLS', () => {
   it('Approach 1: Hard Coded JSON Object ', () => {
     const reqBody = {
       tourist_name: 'Sara J.',
+    //   change email with every call:: 
       tourist_email: 'sara444@gmail.com',
       tourist_location: 'PAS',
     }
